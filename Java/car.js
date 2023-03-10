6a26696df9d13a8c6019c498dfe4738e8aa85f70
NgocPhuc89 <ngocphuc966@gmail.com>
2023-03-10 16:21:21 +0700

    car.html

---
+++ Java/car.js
@@ -0,0 +1,18 @@
+<script>
+        function DOWN(){
+            if(
+                document.getElementsByClassName('down')[0].innerText == "Down"){
+                document.getElementById('car').src='Image/cardown.png'}}
+        function RIGHT(){
+            if(
+                document.getElementsByClassName('right')[0].innerText == "Right"){
+                document.getElementById('car').src='Image/carright.png'}}
+        function LEFT(){
+            if(
+                document.getElementsByClassName('left')[0].innerText == "Left"){
+                document.getElementById('car').src='Image/carleft.png'}}
+        function UP(){
+            if(
+                document.getElementsByClassName('up')[0].innerText == "Up"){
+                document.getElementById('car').src='Image/cartop.png'}}
+    </script>
