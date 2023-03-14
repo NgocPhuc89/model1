175c39a6ebb17c4d6f58bc3ab8129d3851c9fdc2
NgocPhuc89 <ngocphuc966@gmail.com>
2023-03-14 09:12:13 +0700

    showmessage.html

---
+++ Java/showmessage.js
@@ -0,0 +1,13 @@
+
+         function showMessage(){
+            console.log('xin chào!');
+        } 
+        function showMessage(){
+            alert('xin chào');
+        }
+        function showMessage(){
+            document.write('xin chào');
+        } 
+        function showMessage(){
+            document.getElementById('messageDiv').innerHTML = 'xin chào';
+        }
